84f23754b8f85a7ec2aae4522bcdd205df3a02dc
Habil Tabesh <habil.tabesh134@gmail.com>
2022-04-13 19:19:12 -0400

    done

---
+++ app.js
@@ -0,0 +1,39 @@
+
+//Variables are
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+var e = 50;
+var f = 60;
+var g = 70;
+var h = 80;
+var i = 90;
+var j = 100;
+
+
+//Addition
+console.log(a+b);
+console.log(c-d);
+
+//Subtraction
+console.log(a-b);
+console.log(a-d);
+
+//Multiplication
+
+console.log(h*j);
+console.log(f*c);
+
+//Division
+console.log(j/a);
+console.log(h/b);
+
+
+//Incremention
+console.log(f++);
+console.log(d++);
+
+//decrementation
+console.log(a--);
+console.log(f--);
